b4b68e651abfe447498078f3ede198745a296403
fischerlol <fischer-lochlan@iinet.net.au>
2024-04-17 23:25:35 +1000

    Update gear_template_table.sql

--- data/sql/db-world/base/gear_template_table.sql
+++ data/sql/db-world/base/gear_template_table.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS `mod_boost_gear`;
 CREATE TABLE IF NOT EXISTS `mod_boost_gear` (
     `player_class` VARCHAR(50) NOT NULL,
     `player_spec` VARCHAR(50) NOT NULL,
+    `player_race` VARCHAR(50) NOT NULL,
     `equipment_slot` INT(10) UNSIGNED NOT NULL DEFAULT '0',
     `item_entry` INT(10) UNSIGNED NOT NULL DEFAULT '0'
 ) COLLATE='utf8mb4_general_ci' COMMENT='Gear Templates';
